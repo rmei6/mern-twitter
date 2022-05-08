ca03383e8e3cc188a9b0ad9749cb2e79b49a8218
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-05-08 14:50:15 -0400

    got app working locally

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+// import { Route, Redirect, withRouter } from 'react-router-dom';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
 // Passed in from parent component or from mapStateToProps
